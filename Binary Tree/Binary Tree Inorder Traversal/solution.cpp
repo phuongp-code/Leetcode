5564e6f869465add1119e8435644c782de6b5ba0
phuongp-code <phuongpham12062006@gmail.com>
2026-06-28 09:45:46 +0700

    Create solution.cpp

---
+++ Binary Tree/Binary Tree Inorder Traversal/solution.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+   void dfs(TreeNode* root, vector<int>& bt) {
+        if (!root) return;
+        
+        dfs(root->left, bt);
+        bt.push_back(root->val);
+        dfs(root->right, bt);
+    }
+
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> bt;
+        dfs(root, bt);
+        return bt;
+    }
+};
